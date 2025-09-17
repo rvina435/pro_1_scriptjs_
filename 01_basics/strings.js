a5825c6a4ba7b99f2c70698015f6dfe6bdb5eece
Ayushi saini <rvina888222@gmail.com>
2025-09-17 18:57:43 +0000

    strings

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -4,3 +4,10 @@ const repocount=50
 // console.log(name+repocount+"value");
 
 console.log( `hello my name is ${name} and my repocount is${repocount}`);
+
+const gamename=new String('ayushiii');
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.charAt(2));
