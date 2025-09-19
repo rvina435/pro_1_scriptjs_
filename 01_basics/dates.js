40bef74f3fb0b3ee82764826f0e716c920f080ed
Ayushi saini <rvina888222@gmail.com>
2025-09-19 18:03:44 +0000

    date

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,14 +1,19 @@
 //dates
 
-let myDate=new Date()
-console.log(myDate.toString());
-console.log(myDate.toLocaleDateString());
-console.log(typeof myDate);
+// let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
 
-let newdate=new Date(2003,1,21)
-console.log(newdate.toDateString());
-console.log(newdate.toLocaleString());
+// let newdate=new Date(2003,1,21)
+// console.log(newdate.toDateString());
+// console.log(newdate.toLocaleString());
 
 
 let date=new Date("02-21-2003")
-console.log(newdate.toLocaleString());
+// console.log(date.toLocaleString());
+
+// let mytimestamp=Date.now()
+// console.log(mytimestamp);
+// console.log(date.getTime());
+console.log(date.now());
