7f6e2a8bda5614eb4f308b98ba0bba8693121cd2
Ayushi saini <rvina888222@gmail.com>
2025-09-19 14:27:26 +0000

    NUM

---
+++ 01_basics/numandmath.js
@@ -0,0 +1,15 @@
+const score=100
+console.log(score);
+
+const newscore=new Number(100)
+console.log(newscore);
+
+
+console.log(newscore.toString().length);
+console.log(newscore.toFixed(1));
+
+const othernumber=123.1444
+console.log(othernumber.toPrecision(4));
+
+const hii=100000
+console.log(hii.toLocaleString('en-IN'));
