6157b121fddc436ecb9d3015d0e4b5ddc78f7eab
Ayushi saini <rvina888222@gmail.com>
2025-09-24 17:26:13 +0000

    dates

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,19 +1,37 @@
 //dates
 
-// let myDate=new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toLocaleDateString());
-// console.log(typeof myDate);
+ let myDate=new Date()
+ console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
 
-// let newdate=new Date(2003,1,21)
-// console.log(newdate.toDateString());
-// console.log(newdate.toLocaleString());
+ let newdate=new Date(2003,1,21)
+console.log(newdate.toDateString());
+ console.log(newdate.toLocaleString());
 
 
 let date=new Date("02-21-2003")
-// console.log(date.toLocaleString());
+ console.log(date.toLocaleString());
 
-// let mytimestamp=Date.now()
-// console.log(mytimestamp);
-// console.log(date.getTime());
-console.log(date.now());
+let mytimestamp=Date.now()
+console.log(mytimestamp);
+ console.log(date.getTime());
+
+
+console.log(Math.floor(Date.now()/1000));
+let newDate=new Date();
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getHours());
+console.log(newDate.getMinutes());
+console.log(newDate.getSeconds());
+console.log(newDate.getMilliseconds());
+
+
+
+
+
+
+
